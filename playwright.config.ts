b1bda377fc6a71cc08c283be7fe02cd06c0424a9
julien <git@julienc.io>
2024-12-13 22:38:53 +0100

    test: add snapshot tests for qrcode integration

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,9 +71,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'yarn start --port 3000',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    env: { VITE_TESTING_PLAYWRIGHT: 'true' },
+  },
 })
